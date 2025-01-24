b84260a06fba065db8c0a8b00eec6cf0b7f0a87a
Sandesh Shahapur <sandeshshahapur@gmail.com>
2025-01-25 03:32:25 +0530

    moved JoyStick to its own class with better code structure. added align center to instructions.

--- dist/inputHandler.js
+++ dist/inputHandler.js
@@ -1,3 +1,4 @@
+import Joystick from "./JoyStick.js";
 import { Utils } from "./Utils.js";
 export class InputHandler {
     constructor() {
@@ -21,9 +22,8 @@ export class InputHandler {
             window.addEventListener("keyup", (event) => this.handleKeyDownUp(event));
         }
         else {
-            window.addEventListener("touchstart", (event) => this.handleTouchStart(event), { passive: false });
-            window.addEventListener("touchmove", (event) => this.handleTouchStart(event), { passive: false });
-            window.addEventListener("touchend", (event) => this.handleTouchEnd(event));
+            this.joystick = new Joystick(this.keys);
+            this.joystick.setupTouchListeners();
         }
     }
     handleKeyDownUp(event) {
@@ -34,84 +34,6 @@ export class InputHandler {
             event.preventDefault();
             this.keys[key] = event.type === "keydown";
         }
-    }
-    handleTouchStart(event) {
-        const E = event.touches[0];
-        if (!(E.target instanceof HTMLElement) ||
-            E.target.id !== "joyStickBall")
-            return;
-        event.preventDefault();
-        this.moveJoyStickBall(E);
-    }
-    handleTouchMove(event) {
-        const E = event.touches[0];
-        if (!(E.target instanceof HTMLElement) ||
-            E.target.id !== "joyStickBall")
-            return;
-        event.preventDefault();
-        this.moveJoyStickBall(E);
-    }
-    handleTouchEnd(event) {
-        const E = event.changedTouches[0];
-        if (!(E.target instanceof HTMLElement) ||
-            E.target.id !== "joyStickBall")
-            return;
-        event.preventDefault();
-        this.resetJoyStickBall();
-    }
-    moveJoyStickBall(E) {
-        const joyStickBall = document.getElementById("joyStickBall");
-        const joyStickRing = document.getElementById("joyStickRing");
-        const ringRect = joyStickRing.getBoundingClientRect();
-        const ringCenterX = ringRect.left + ringRect.width / 2;
-        const ringCenterY = ringRect.top + ringRect.height / 2;
-        const touchX = E.clientX - ringCenterX;
-        const touchY = E.clientY - ringCenterY;
-        joyStickBall.style.left = `${touchX}px`;
-        joyStickBall.style.top = `${touchY}px`;
-        const angle_rad = Math.atan2(touchY, touchX);
-        const angle_deg = angle_rad * (180 / Math.PI);
-        const normalized_angle = (-1 * (angle_deg - 360 - 22.5)) % 360;
-        const direction = Math.floor(normalized_angle / (360 / 8));
-        this.resetKeys();
-        switch (direction) {
-            case 0:
-                this.keys["ArrowRight"] = true;
-                break;
-            case 1:
-                this.keys["ArrowRight"] = true;
-                this.keys["ArrowUp"] = true;
-                break;
-            case 2:
-                this.keys["ArrowUp"] = true;
-                break;
-            case 3:
-                this.keys["ArrowUp"] = true;
-                this.keys["ArrowLeft"] = true;
-                break;
-            case 4:
-                this.keys["ArrowLeft"] = true;
-                break;
-            case 5:
-                this.keys["ArrowLeft"] = true;
-                this.keys["ArrowDown"] = true;
-                break;
-            case 6:
-                this.keys["ArrowDown"] = true;
-                break;
-            case 7:
-                this.keys["ArrowDown"] = true;
-                this.keys["ArrowRight"] = true;
-                break;
-            default:
-                break;
-        }
-    }
-    resetJoyStickBall() {
-        const joyStickBall = document.getElementById("joyStickBall");
-        joyStickBall.style.left = "auto";
-        joyStickBall.style.top = "auto";
-        this.resetKeys();
     }
     isKeyPressed(key) {
         return !!this.keys[key];
--- src/InputHandler.ts
+++ src/InputHandler.ts
@@ -1,8 +1,10 @@
+import Joystick from "./JoyStick.js";
 import { Utils } from "./Utils.js";
 
 export class InputHandler {
     private keys: Record<string, boolean>;
     private altKeys: Record<string, string>;
+    private joystick: Joystick;
 
     constructor() {
         this.keys = {
@@ -11,12 +13,14 @@ export class InputHandler {
             ArrowLeft: false,
             ArrowRight: false,
         };
+
         this.altKeys = {
             w: "ArrowUp",
             s: "ArrowDown",
             a: "ArrowLeft",
             d: "ArrowRight",
         };
+
         this.setupEventListeners();
     }
 
@@ -29,19 +33,8 @@ export class InputHandler {
                 this.handleKeyDownUp(event)
             );
         } else {
-            window.addEventListener(
-                "touchstart",
-                (event) => this.handleTouchStart(event),
-                { passive: false }
-            );
-            window.addEventListener(
-                "touchmove",
-                (event) => this.handleTouchStart(event),
-                { passive: false }
-            );
-            window.addEventListener("touchend", (event) =>
-                this.handleTouchEnd(event)
-            );
+            this.joystick = new Joystick(this.keys);
+            this.joystick.setupTouchListeners();
         }
     }
 
@@ -54,110 +47,6 @@ export class InputHandler {
             event.preventDefault();
             this.keys[key] = event.type === "keydown";
         }
-    }
-
-    handleTouchStart(event: TouchEvent) {
-        const E = event.touches[0];
-        if (
-            !(E.target instanceof HTMLElement) ||
-            E.target.id !== "joyStickBall"
-        )
-            return;
-
-        event.preventDefault();
-        this.moveJoyStickBall(E);
-    }
-
-    handleTouchMove(event: TouchEvent) {
-        const E = event.touches[0];
-        if (
-            !(E.target instanceof HTMLElement) ||
-            E.target.id !== "joyStickBall"
-        )
-            return;
-
-        event.preventDefault();
-        this.moveJoyStickBall(E);
-    }
-
-    handleTouchEnd(event: TouchEvent) {
-        const E = event.changedTouches[0];
-        if (
-            !(E.target instanceof HTMLElement) ||
-            E.target.id !== "joyStickBall"
-        )
-            return;
-
-        event.preventDefault();
-        this.resetJoyStickBall();
-    }
-
-    moveJoyStickBall(E: Touch) {
-        const joyStickBall = document.getElementById(
-            "joyStickBall"
-        ) as HTMLElement;
-        const joyStickRing = document.getElementById(
-            "joyStickRing"
-        ) as HTMLElement;
-
-        const ringRect = joyStickRing.getBoundingClientRect();
-        const ringCenterX = ringRect.left + ringRect.width / 2;
-        const ringCenterY = ringRect.top + ringRect.height / 2;
-
-        const touchX = E.clientX - ringCenterX;
-        const touchY = E.clientY - ringCenterY;
-
-        joyStickBall.style.left = `${touchX}px`;
-        joyStickBall.style.top = `${touchY}px`;
-
-        const angle_rad = Math.atan2(touchY, touchX);
-        const angle_deg = angle_rad * (180 / Math.PI);
-        const normalized_angle = (-1 * (angle_deg - 360 - 22.5)) % 360;
-
-        const direction = Math.floor(normalized_angle / (360 / 8));
-        this.resetKeys();
-        switch (direction) {
-            case 0:
-                this.keys["ArrowRight"] = true;
-                break;
-            case 1:
-                this.keys["ArrowRight"] = true;
-                this.keys["ArrowUp"] = true;
-                break;
-            case 2:
-                this.keys["ArrowUp"] = true;
-                break;
-            case 3:
-                this.keys["ArrowUp"] = true;
-                this.keys["ArrowLeft"] = true;
-                break;
-            case 4:
-                this.keys["ArrowLeft"] = true;
-                break;
-            case 5:
-                this.keys["ArrowLeft"] = true;
-                this.keys["ArrowDown"] = true;
-                break;
-            case 6:
-                this.keys["ArrowDown"] = true;
-                break;
-            case 7:
-                this.keys["ArrowDown"] = true;
-                this.keys["ArrowRight"] = true;
-                break;
-            default:
-                break;
-        }
-    }
-
-    resetJoyStickBall() {
-        const joyStickBall = document.getElementById(
-            "joyStickBall"
-        ) as HTMLElement;
-        joyStickBall.style.left = "auto";
-        joyStickBall.style.top = "auto";
-
-        this.resetKeys();
     }
 
     isKeyPressed(key: string): boolean {
